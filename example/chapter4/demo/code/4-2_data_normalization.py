8a3fa55f4d7041369264c7936d553e8c58dcf4ef
邢维祥 <xingwx@xjj.com>
2019-05-27 13:16:12 +0800

    数据规范化

---
+++ example/chapter4/demo/code/4-2_data_normalization.py
@@ -0,0 +1,12 @@
+﻿#-*- coding: utf-8 -*-
+#数据规范化
+import pandas as pd
+import numpy as np
+
+datafile = '../data/normalization_data.xls' #参数初始化
+data = pd.read_excel(datafile, header = None) #读取数据
+
+a = (data - data.min())/(data.max() - data.min()) #最小-最大规范化
+b = (data - data.mean())/data.std() #零-均值规范化
+c = data/10**np.ceil(np.log10(data.abs().max())) #小数定标规范化
+print(c)
