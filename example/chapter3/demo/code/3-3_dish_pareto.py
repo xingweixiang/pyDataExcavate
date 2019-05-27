c003d7354cb0c4d57f682e5bdb1df11f1452ac53
邢维祥 <xingwx@xjj.com>
2019-05-27 13:10:07 +0800

    帕累托图

---
+++ example/chapter3/demo/code/3-3_dish_pareto.py
@@ -0,0 +1,26 @@
+#-*- coding: utf-8 -*-
+#菜品盈利数据 帕累托图
+from __future__ import print_function
+import pandas as pd
+
+#初始化参数
+dish_profit = '../data/catering_dish_profit.xls' #餐饮菜品盈利数据
+data = pd.read_excel(dish_profit, index_col = u'菜品名')
+print(data)
+data = data[u'盈利'].copy()
+print('------------------')
+print(data)
+#data.sort(ascending = False) 会提示AttributeError: 'Series' object has no attribute 'sort'也就是说Series没有sorted这个方法 应该这样：sorted(.....) ...是你要排序的Seri
+data.sort_values(ascending = False) #可以用Series.sort_values()方法,对Series值进行排序。
+import matplotlib.pyplot as plt #导入图像库
+plt.rcParams['font.sans-serif'] = ['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False #用来正常显示负号
+
+plt.figure()
+data.plot(kind='bar')
+plt.ylabel(u'盈利（元）')
+p = 1.0*data.cumsum()/data.sum()
+p.plot(color = 'r', secondary_y = True, style = '-o',linewidth = 2)
+plt.annotate(format(p[6], '.4%'), xy = (6, p[6]), xytext=(6*0.9, p[6]*0.9), arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2")) #添加注释，即85%处的标记。这里包括了指定箭头样式。
+plt.ylabel(u'盈利（比例）')
+plt.show()
