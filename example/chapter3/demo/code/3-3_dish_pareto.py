79e893d67d6a226f53ded370e81d7957d8bcd285
Administrator <281349503@qq.com>
2019-06-01 00:20:04 +0800

    菜品盈利数据 帕累托图

--- example/chapter3/demo/code/3-3_dish_pareto.py
+++ example/chapter3/demo/code/3-3_dish_pareto.py
@@ -24,4 +24,5 @@ p = 1.0*data.cumsum()/data.sum()
 p.plot(color = 'r', secondary_y = True, style = '-o',linewidth = 2)
 plt.annotate(format(p[6], '.4%'), xy = (6, p[6]), xytext=(6*0.9, p[6]*0.9), arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2")) #添加注释，即85%处的标记。这里包括了指定箭头样式。
 plt.ylabel(u'盈利（比例）')
+#plt.savefig('../data/plt.jpg')
 plt.show()
